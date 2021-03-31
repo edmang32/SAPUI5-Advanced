b2790ab5f2488562524b8e3f94d2c2b9961010a7
edmang32 <edgmang32@gmail.com>
2021-03-31 17:17:59 +0000

    Commit Modelos y Eventos

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,28 +1,63 @@
+// @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
-        var Main = Controller.extend("ns.Employees.controller.MainView", {})
-        //onInit: function () {},
+        function onInit() {
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            oJSONModel.loadData("./localService/mockdata/Employees.json", false);
+            oView.setModel(oJSONModel);
+        };
 
-        Main.prototype.onValidate = function () {
-            var inputEmployee = this.byId("inputEmployee");
-            var valueEmployee = inputEmployee.getValue();
+        function onFilter() {
+            var oJSON = this.getView().getModel().getData();
 
-            if (valueEmployee.length === 6) {
-                //inputEmployee.setDescription("OK");
-                this.getView().byId("labelCountry").setVisible(true);
-                this.getView().byId("slCountry").setVisible(true);
-            } else {
-                //inputEmployee.setDescription("Not OK");
-                this.getView().byId("labelCountry").setVisible(false);
-                this.getView().byId("slCountry").setVisible(false);
+            var filters = [];
+            if (oJSON.EmployeeId !== ""){
+                filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId ));
             }
+
+            if (oJSON.CountryKey !== ""){
+                filters.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey ));
+            }
+
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+        };
+
+        function onClearFilter() {
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "")
+            oModel.setProperty("/CountryKey", "")
         }
+
+        var Main = Controller.extend("ns.Employees.controller.MainView", {})
+        // Main.prototype.onValidate = function () {
+        //     var inputEmployee = this.byId("inputEmployee");
+        //     var valueEmployee = inputEmployee.getValue();
+
+        //     if (valueEmployee.length === 6) {
+        //         //inputEmployee.setDescription("OK");
+        //         this.getView().byId("labelCountry").setVisible(true);
+        //         this.getView().byId("slCountry").setVisible(true);
+        //     } else {
+        //         //inputEmployee.setDescription("Not OK");
+        //         this.getView().byId("labelCountry").setVisible(false);
+        //         this.getView().byId("slCountry").setVisible(false);
+        //     }
+        // };
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter;
+        Main.prototype.onClearFilter = onClearFilter;
+
         return Main;
     });
